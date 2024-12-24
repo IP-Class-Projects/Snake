c83629d39c9558680e0447e0e2a8a418de1ea473
Robeill <robeldesalegn07@gmail.com>
2024-12-24 18:52:48 +0300

    Add landing page

---
+++ js/config.js
@@ -0,0 +1,12 @@
+export const GAME_CONFIG = {
+    CANVAS_SIZE: 800,
+    TILE_SIZE: 25,
+    INITIAL_SPEED: 150, // Reduced speed for faster movement
+    COLORS: {
+        SNAKE_HEAD: '#50C878',
+        SNAKE_HEAD_DARK: '#228B22',
+        SNAKE_BODY: '#3CB371',
+        SNAKE_BODY_DARK: '#2E8B57',
+        APPLE: '#ff3b30'
+    }
+};
